cef757da6d6da99971ec8647f035f4fe0e47f767
Heshie Brody <4hbrody@gmail.com>
2021-05-24 00:49:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,17 @@ class Person
     puts("Hello World!")
   end
 
-  def walk
-    puts("The Person is walking")
+  def walk(name)
+    puts("#{name} is walking")
+  end
+
+  def name(name)
+    name
   end
 end
 
 moti = Person.new
 
-moti
+moti.name("Moti")
 
-moti.walk
+moti.walk(moti.name)
